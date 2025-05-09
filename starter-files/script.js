89cf4e1007013cbd77aef915fa820a1a5f34513b
Ricardo MR <ricardomr.23@hotmail.com>
2025-05-08 19:14:52 -0600

    Se sube el ejercicio resuelto

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,13 +1,49 @@
-// API
+const answer = document.getElementById('answer');
+const button = document.getElementById('button');
+const inputEntrada = document.getElementById('inputEntrada');
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
-/**
- * STEPS:
- *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
- * 4. Clear output after 3 seconds
- * 5. Optional: add loading/error states
- *
- */
+// Creacion de la promesa y fetch de api ->
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        fetch(API_ENDPOINT)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ocurrio un error al obtener los datos');
+                } else {
+                    return response.json();
+                }
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(error));
+    });
+};
+
+// funcion con toda la logica, reenderizar datos, limpieza de campos, manejo de errores ->
+const fetchAnswer = async () => {
+    button.addEventListener('click', async event => {
+        event.preventDefault();
+
+        const inputValor = inputEntrada.value;
+        if (inputValor === '') {
+            button.disabled;
+        } else {
+            try {
+                const responseApi = await getData();
+                answer.innerHTML = `
+                    <div class="body-elemento">
+                        <p>${responseApi.answer}
+                    </div>
+                `;
+                setTimeout(() => {
+                    inputEntrada.value = '';
+                    answer.innerText = '';
+                    inputEntrada.focus();
+                }, 5000);
+            } catch (error) {
+                answer.innerText = 'Error';
+            }
+        }
+    });
+};
+fetchAnswer();
